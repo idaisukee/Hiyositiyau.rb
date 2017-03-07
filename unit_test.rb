3e0b73e56c855a0bce9c3c55c74578ec7ee64da0
Daisuke Inoue <hibariyashiki@gmail.com>
2017-03-07 17:29:38 +0900

    testcase

--- unit_test.rb
+++ unit_test.rb
@@ -15,7 +15,16 @@ class TC < Test::Unit::TestCase
 	end
 
 	def test_rd_is_independent_from_time_offest
-		assert_equal(Hiyositiyau::EPOCH.rd, Hiyositiyau::EPOCH.new_offset(0.3).rd)
+		(0...100).each do |i|
+			assert_equal(Hiyositiyau::EPOCH.rd, Hiyositiyau::EPOCH.new_offset(i / 100.0).rd)
+		end
+	end
+
+	def test_rd_is_correct_for_adding
+
+		(-100_000..100_000).each do |i|
+			assert_equal((Hiyositiyau::EPOCH + i).rd, Hiyositiyau::EPOCH.rd + i)
+		end
 	end
 
 end
