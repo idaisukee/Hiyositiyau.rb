21e9d22911ff14d37a7462b20925e4e171928440
Daisuke Inoue <hibariyashiki@gmail.com>
2017-03-07 19:52:38 +0900

    from_ajd, from_rd.

--- Hiyositiyau.rb
+++ Hiyositiyau.rb
@@ -8,6 +8,21 @@ class Hiyositiyau < DateTime
 
 	JST = Rational(9, 24)
 
+	def self::from_ajd(ajd)
+		@ajd = ajd
+		@jd = @ajd + Rational(1, 2)
+		self.jd(@jd)
+	end
+
+
+	def self::from_rd(rd)
+		@rd = rd
+		@ajd = self::EPOCH.ajd + @rd
+		self::from_ajd(@ajd)
+	end
+
+
+
 	def rd
 		self.ajd - EPOCH.ajd
 	end
