ab65a77325e8db6e9ead9b8e465324fc24948b29
Daisuke Inoue <hibariyashiki@gmail.com>
2017-09-29 22:23:38 +0900

    otohatiyau

--- Hiyositiyau.rb
+++ Hiyositiyau.rb
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+require 'bundler/setup'
+Bundler.require
+
+p Otohatiyau::otohatiyau(500, [7, 5, 3])
+
 require 'date'
 
 class Hiyositiyau < DateTime
@@ -7,6 +12,17 @@ class Hiyositiyau < DateTime
 	EPOCH = self.new(1792, 9, 22, 0, 0, 0, Rational(1, 24))
 
 	JST = Rational(9, 24)
+
+	Y = 365
+
+	# 4 years
+	A = 4 * Y + 1
+
+	# 100 years
+	B = 25 * A - 1
+
+	# 400 years
+	C = 4 * B + 1
 
 	def self::from_ajd(ajd)
 		@ajd = ajd
@@ -27,6 +43,16 @@ class Hiyositiyau < DateTime
 		self.ajd - EPOCH.ajd
 	end
 
-end
+	def self::from_gc(year, year_day)
 
-		
+		otohatiyau = Otohatiyau::otohatiyau(year, [400, 100, 4])
+		c = otohatiyau[0][0]
+		b = otohatiyau[0][1]
+		a = otohatiyau[0][2]
+		y = otohatiyau[1][2]
+		days = C * c + B * b + A * a + Y * y
+		sum = days + year_day
+	end
+
+
+end
